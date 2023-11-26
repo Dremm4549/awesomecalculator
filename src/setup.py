9dd28d704cc2166ebf6f02455534961f1805e30e
dremm4549 <mDremo5093@conestogac.on.ca>
2023-11-26 11:30:24 -0500

    Test

--- src/setup.py
+++ src/setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approve :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
