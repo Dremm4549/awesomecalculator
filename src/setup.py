e603748c4277761bb18979dc1b2834141ff0d129
dremm4549 <mDremo5093@conestogac.on.ca>
2023-11-26 11:28:21 -0500

    added pytest package

--- src/setup.py
+++ src/setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="dedreira-awesomecalculator", # Replace with your own username
+    name="dremo-awesomecalculator", # Replace with your own username
     version= "1.0.0",
-    author="Daniel Edreira",
+    author="Michael",
     author_email="dedreira@github.com",
-    description="An awesome calculator package",
+    description="An awesome calculator packagee",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dedreira/awesomecalculator",
